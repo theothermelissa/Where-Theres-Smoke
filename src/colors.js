36f2187fef4d0d5fed2f915907c2edd6f80e74cf
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-03-12 13:46:26 -0500

    Updates webpack server config

---
+++ src/colors.js
@@ -0,0 +1,10 @@
+export const lightestgray = '#EFEFEF';
+export const mediumgray = '#B5B5B5';
+export const charcoal = '#42413E';
+export const mint = '#BAE8DD';
+export const lightmint = '#E1F4EE';
+export const coral = '#F29F9E';
+export const lightcoral = '#F4D3D3';
+export const cream = '#F4E7D7';
+
+// lightestgray, mediumgray, charcoal, mint, lightmint, coral, lightcoral, cream
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
-{
+const path = require("path");
+
+module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
     filename: './main.js'
   },
-}
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    watchContentBase: true,
+    progress: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|giv)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
